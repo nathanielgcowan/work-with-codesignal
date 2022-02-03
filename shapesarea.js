27c7a065766a06f6a2a69a53172ba869e35b8670
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2022-02-02 22:15:40 -0600

    shapearea

--- shapesarea.js
+++ shapesarea.js
@@ -1,7 +1,9 @@
 function name(array) {
-    var count = 1;
-    for ( i = 0 ; i < array.length; i++) {
+    var count = 1
+    
+    for(i=0; i < array; i++) {
         count += 4 * i
     }
+    
     return count
 }
